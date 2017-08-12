90efce20d59330f47fb9acacf39d57948d5bda9b
Nicholas Carr <nicholas.carr@rogers.com>
2017-08-12 03:23:18 -0400

    Added port variable

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
 
+const port = process.env.PORT || 8000
+
 const app = express()
 app.use('/', express.static('public'))
-app.listen(8000)
+app.listen(port)
 
-console.log('Open localhost:8000 in your browser');
+console.log(`Open localhost:${port} in your browser`);
