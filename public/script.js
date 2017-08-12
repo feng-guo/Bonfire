5fe2ccee5f9732644cce8f4c73868b0cd50c1e00
Nicholas Carr <nicholas.carr@rogers.com>
2017-08-12 15:27:28 -0400

    Finds nearby thingies

--- public/script.js
+++ public/script.js
@@ -1,21 +1,41 @@
 var app = new Vue({
-  el: '#app',
+  el: '#content',
   data: {
-    proposals: null
+    proposals: null,
+    hasGeolocation: false
   },
   created() {
+    this.hasGeolocation = "geolocation" in navigator
     axios.get('https://services3.arcgis.com/rl7ACuZkiFsmDA2g/arcgis/rest/services/Planning_Land_Use_Development/FeatureServer/2/query', {
       params: {
         outFields: 'DATE_RECEI,DESCRIPTIO,PROPOSAL_D',
-        where: "STATUS IN ('In progress', 'Draft Approved', 'Public meeting', 'Received')",
+        where: "STATUS = 'In progress'",
         orderByFields: 'DATE_RECEI DESC',
         f: 'json',
         returnGeometry: false
       }
     })
-    .then(res => {
-      console.log(res.data);
-      this.proposals = res.data
-    })
+    .then(res => this.proposals = res.data)
+  },
+  methods: {
+    getLocation() {
+      navigator.geolocation.getCurrentPosition(position => {
+        axios.get('https://services3.arcgis.com/rl7ACuZkiFsmDA2g/arcgis/rest/services/Planning_Land_Use_Development/FeatureServer/2/query', {
+          params: {
+            outFields: 'DATE_RECEI,DESCRIPTIO,PROPOSAL_D',
+            where: `STATUS = 'In progress'`,
+            geometryType: 'esriGeometryEnvelope',
+            geometry: `${position.coords.longitude-0.02},${position.coords.latitude-0.02},${position.coords.longitude+0.02},${position.coords.latitude+0.02}`,
+            returnGeometry: false,
+            inSR: 4326,
+            spatialRel: 'esriSpatialRelIntersects',
+            outSR: 4326,
+            orderByFields: 'DATE_RECEI DESC',
+            f: 'json'
+          }
+        })
+        .then(res => this.proposals = res.data)
+      })
+    }
   }
 })
